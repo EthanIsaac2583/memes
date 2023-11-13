fdb7fa9ceb39cf11af105eaafb73623035d5283f
eitan.aizek <eitan.aizek@techork.com>
2023-11-13 18:35:31 +0500

    m: fixed file formates

--- src/main/java/kz/ruanjian/memed/Main.java
+++ src/main/java/kz/ruanjian/memed/Main.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemedApplication {
+public class Main {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemedApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
 }
