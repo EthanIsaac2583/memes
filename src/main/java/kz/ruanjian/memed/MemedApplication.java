c82c88010cb5198fe2949f5dc63c0e60e87eebb1
eitan.aizek <eitan.aizek@techork.com>
2023-11-13 18:26:44 +0500

    first commit

---
+++ src/main/java/kz/ruanjian/memed/MemedApplication.java
@@ -0,0 +1,13 @@
+package kz.ruanjian.memed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/ruanjian/memed/MemedApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ruanjian.memed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
