b569e351b9b3a5df0d112b2d64a6fa1d1e7c5f3d
eitan.aizek <eitan.aizek@techork.com>
2023-11-15 20:33:45 +0500

    m: temporary removed shallow deletion of task

--- memed-core/src/main/resources/db/migration/V0_0_2__questions.sql
+++ memed-core/src/main/resources/db/migration/V0_0_2__questions.sql
@@ -5,6 +5,5 @@ create table tasks
   description varchar(512) null,
   question    varchar      not null,
   blank       varchar      not null,
-  answer      varchar      not null,
-  is_deleted  boolean      not null
+  answer      varchar      not null
 );
