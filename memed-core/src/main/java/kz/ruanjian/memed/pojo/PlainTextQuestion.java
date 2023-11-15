9f9a71050205a6e9b8430dd3d9ac619ecbefe345
eitan.aizek <eitan.aizek@techork.com>
2023-11-15 17:15:56 +0500

    m: added PlainTextQuestion

---
+++ memed-core/src/main/java/kz/ruanjian/memed/pojo/PlainTextQuestion.java
@@ -0,0 +1,33 @@
+package kz.ruanjian.memed.pojo;
+
+import java.util.Objects;
+
+public class PlainTextQuestion extends Question {
+
+  private String question;
+
+  public PlainTextQuestion() {
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    PlainTextQuestion that = (PlainTextQuestion) o;
+    return Objects.equals(question, that.question);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), question);
+  }
+}
