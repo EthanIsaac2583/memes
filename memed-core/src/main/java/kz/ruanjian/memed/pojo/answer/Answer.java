7cc61f22cc43705237f2a6f6cdb17c109f4ca9c8
eitan.aizek <eitan.aizek@techork.com>
2023-11-15 17:32:42 +0500

    m: added SingleChoiceAnswer

---
+++ memed-core/src/main/java/kz/ruanjian/memed/pojo/answer/Answer.java
@@ -0,0 +1,41 @@
+package kz.ruanjian.memed.pojo.answer;
+
+import kz.ruanjian.memed.pojo.BlankType;
+
+import java.util.Objects;
+
+public class Answer {
+
+  protected BlankType type;
+
+  public Answer() {
+  }
+
+  public BlankType getType() {
+    return type;
+  }
+
+  public void setType(BlankType type) {
+    this.type = type;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    Answer answer = (Answer) o;
+    return type==answer.type;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(type);
+  }
+
+  @Override
+  public String toString() {
+    return "Answer{" +
+      "type=" + type +
+      '}';
+  }
+}
---
+++ memed-core/src/main/java/kz/ruanjian/memed/pojo/answer/SingleChoiceAnswer.java
@@ -0,0 +1,41 @@
+package kz.ruanjian.memed.pojo.answer;
+
+import java.util.Objects;
+
+public class SingleChoiceAnswer extends Answer {
+
+  private String key;
+
+  public SingleChoiceAnswer() {
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    SingleChoiceAnswer that = (SingleChoiceAnswer) o;
+    return Objects.equals(key, that.key);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), key);
+  }
+
+  @Override
+  public String toString() {
+    return "SingleChoiceAnswer{" +
+      "key='" + key + '\'' +
+      ", type=" + type +
+      '}';
+  }
+}
