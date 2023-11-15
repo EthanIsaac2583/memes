56799d9fa7c63f74f8ac80016edc95eec85b3716
eitan.aizek <eitan.aizek@techork.com>
2023-11-15 17:38:20 +0500

    m: added shallow delete flag in tasks sql

--- memed-core/src/main/java/kz/ruanjian/memed/model/Task.java
+++ memed-core/src/main/java/kz/ruanjian/memed/model/Task.java
@@ -13,4 +13,10 @@ public class Task {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  private String name;
+
+  private String description;
+
+
 }
--- memed-core/src/main/resources/db/migration/V0_0_2__questions.sql
+++ memed-core/src/main/resources/db/migration/V0_0_2__questions.sql
@@ -5,5 +5,6 @@ create table tasks
   description varchar(512) null,
   question    varchar      not null,
   blank       varchar      not null,
-  answer      varchar      not null
+  answer      varchar      not null,
+  is_deleted  boolean      not null
 );
