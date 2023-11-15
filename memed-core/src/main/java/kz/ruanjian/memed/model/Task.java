5a15d92ead9e59175018c224938860cc9af18370
eitan.aizek <eitan.aizek@techork.com>
2023-11-15 18:13:03 +0500

    m: added Task

--- memed-core/src/main/java/kz/ruanjian/memed/model/Task.java
+++ memed-core/src/main/java/kz/ruanjian/memed/model/Task.java
@@ -6,8 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import kz.ruanjian.memed.pojo.answer.Answer;
+import kz.ruanjian.memed.pojo.blank.Blank;
+import kz.ruanjian.memed.pojo.coverter.AnswerConverter;
+import kz.ruanjian.memed.pojo.coverter.BlankConverter;
 import kz.ruanjian.memed.pojo.coverter.QuestionConverter;
 import kz.ruanjian.memed.pojo.quiestion.Question;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "tasks")
@@ -23,4 +29,86 @@ public class Task {
 
   @Convert(converter = QuestionConverter.class)
   private Question question;
+
+  @Convert(converter = BlankConverter.class)
+  private Blank blank;
+
+  @Convert(converter = AnswerConverter.class)
+  private Answer answer;
+
+  public Task() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Question getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(Question question) {
+    this.question = question;
+  }
+
+  public Blank getBlank() {
+    return blank;
+  }
+
+  public void setBlank(Blank blank) {
+    this.blank = blank;
+  }
+
+  public Answer getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(Answer answer) {
+    this.answer = answer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    Task task = (Task) o;
+    return Objects.equals(id, task.id) && Objects.equals(name, task.name) && Objects.equals(description, task.description) && Objects.equals(question, task.question) && Objects.equals(blank, task.blank) && Objects.equals(answer, task.answer);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, description, question, blank, answer);
+  }
+
+  @Override
+  public String toString() {
+    return "Task{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", description='" + description + '\'' +
+      ", question=" + question +
+      ", blank=" + blank +
+      ", answer=" + answer +
+      '}';
+  }
 }
