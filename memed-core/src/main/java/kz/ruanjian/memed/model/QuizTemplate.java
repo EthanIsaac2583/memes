6214e38c87c3dce14d7fc03f87020d8affba2176
eitan.aizek <eitan.aizek@techork.com>
2023-11-18 04:24:19 +0500

    m: added QuizTemplate

---
+++ memed-core/src/main/java/kz/ruanjian/memed/model/QuizTemplate.java
@@ -0,0 +1,80 @@
+package kz.ruanjian.memed.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "quiz_templates")
+public class QuizTemplate {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @ManyToMany
+  @JoinTable(name = "quiz_templates_questions",
+    joinColumns = @JoinColumn(name = "quiz_template_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "question_id", referencedColumnName = "id"))
+  private Set<Task> tasks;
+
+  public QuizTemplate() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(Set<Task> tasks) {
+    this.tasks = tasks;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    QuizTemplate that = (QuizTemplate) o;
+    return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(tasks, that.tasks);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, tasks);
+  }
+
+  @Override
+  public String toString() {
+    return "QuizTemplate{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", tasks=" + tasks +
+      '}';
+  }
+}
