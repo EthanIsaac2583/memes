e7dfadd8dd5f7e84a0117f107678a3e274a1cbb8
eitan.aizek <eitan.aizek@techork.com>
2023-11-20 23:58:39 +0500

    m: added LeadQuizAnswerController

---
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/LeadQuizAnswerController.java
@@ -0,0 +1,43 @@
+package kz.ruanjian.memed.controller;
+
+import jakarta.validation.Valid;
+import kz.ruanjian.memed.dto.LeadQuizAnswerGradeDto;
+import kz.ruanjian.memed.dto.LeadQuizAnswerProvideDto;
+import kz.ruanjian.memed.model.LeadQuizAnswer;
+import kz.ruanjian.memed.service.LeadQuizAnswerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/lead-quiz-answers")
+public class LeadQuizAnswerController {
+
+  private final LeadQuizAnswerService leadQuizAnswerService;
+
+  public LeadQuizAnswerController(LeadQuizAnswerService leadQuizAnswerService) {
+    this.leadQuizAnswerService = leadQuizAnswerService;
+  }
+
+  @GetMapping("/{id}")
+  public LeadQuizAnswer findById(@PathVariable Long id) {
+    return leadQuizAnswerService.findById(id);
+  }
+
+  @PutMapping("/{id}/provide")
+  public void provide(@PathVariable Long id,
+                      @RequestBody @Valid LeadQuizAnswerProvideDto leadQuizAnswer) {
+    leadQuizAnswer.setId(id);
+    leadQuizAnswerService.provide(leadQuizAnswer);
+  }
+
+  @PutMapping("/{id}/grade")
+  public void grade(@PathVariable Long id,
+                    @RequestBody @Valid LeadQuizAnswerGradeDto leadQuizAnswer) {
+    leadQuizAnswer.setId(id);
+    leadQuizAnswerService.grade(leadQuizAnswer);
+  }
+}
