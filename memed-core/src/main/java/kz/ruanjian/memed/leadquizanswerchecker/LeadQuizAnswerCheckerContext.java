fe47e8b0ad6c35098c688fd7fdc1b369b345aa6f
eitan.aizek <eitan.aizek@techork.com>
2023-11-21 00:14:47 +0500

    m: used MemedProperties

--- memed-core/src/main/java/kz/ruanjian/memed/leadquizanswerchecker/LeadQuizAnswerCheckerContext.java
+++ memed-core/src/main/java/kz/ruanjian/memed/leadquizanswerchecker/LeadQuizAnswerCheckerContext.java
@@ -1,13 +1,20 @@
 package kz.ruanjian.memed.leadquizanswerchecker;
 
+import kz.ruanjian.memed.config.MemedProperties;
 import kz.ruanjian.memed.model.LeadQuizAnswer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LeadQuizAnswerCheckerContext implements LeadQuizAnswerChecker {
 
+  private final MemedProperties memedProperties;
+
+  public LeadQuizAnswerCheckerContext(MemedProperties memedProperties) {
+    this.memedProperties = memedProperties;
+  }
+
   @Override
   public int check(LeadQuizAnswer answer) {
-    return 100;
+    return memedProperties.getGradeMax();
   }
 }
