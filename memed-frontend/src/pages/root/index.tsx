981f5c62afb301ec45afcf0b93fbd380906ffc88
eitan.aizek <eitan.aizek@techork.com>
2023-11-21 17:37:11 +0500

    m: first fetch

--- memed-frontend/src/pages/root/index.tsx
+++ memed-frontend/src/pages/root/index.tsx
@@ -1,6 +1,18 @@
 import {BaseLayout} from "../../components/base-layout";
+import {useEffect} from "react";
+import axios from "axios";
 
 export const RootPage = () => {
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:8080/api/v1/quiz-templates/1'
+    }).then((response) => {
+      console.log('--------> response', response.data);
+    })
+  }, []);
+
   return (
     <BaseLayout>
       <div>Root page</div>
