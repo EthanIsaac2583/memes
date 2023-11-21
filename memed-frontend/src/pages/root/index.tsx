71b19402abb255f7f3b63139f50d2d54423ffd58
eitan.aizek <eitan.aizek@techork.com>
2023-11-21 17:07:32 +0500

    m: used router Link

--- memed-frontend/src/pages/root/index.tsx
+++ memed-frontend/src/pages/root/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from '../../assets/logo.svg';
 
 export const RootPage = () => {
@@ -5,9 +6,9 @@ export const RootPage = () => {
     <header className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <nav className="container-xxl" aria-label="Main navigation">
 
-        <a className="navbar-brand" href="/" aria-label="Boosted">
+        <Link className="navbar-brand" to="/" aria-label="Boosted">
           <img src={Logo} alt="logo" />
-        </a>
+        </Link>
 
         <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -16,10 +17,10 @@ export const RootPage = () => {
         <div className="navbar-collapse collapse" id="bdNavbar">
           <ul className="navbar-nav flex-row flex-wrap bd-navbar-nav pt-2 py-md-0">
             <li className="nav-item col-6 col-md-auto">
-              <a className="nav-link" href="/">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item col-6 col-md-auto">
-              <a className="nav-link" href="/about">About</a>
+              <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
         </div>
