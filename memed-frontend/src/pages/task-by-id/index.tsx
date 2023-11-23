9241cfa06f8351e40724d2448a075c3f6122acb9
eitan.aizek <eitan.aizek@techork.com>
2023-11-23 21:57:54 +0500

    m: fetching question by id

--- memed-frontend/src/pages/task-by-id/index.tsx
+++ memed-frontend/src/pages/task-by-id/index.tsx
@@ -1,4 +1,20 @@
+import {useParams} from "react-router-dom";
+import {useEffect} from "react";
+import axios from "axios";
+
 export const TaskById = () => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `http://192.168.100.5:8080/api/v1/tasks/${id}`
+    })
+      .then(response => {
+        console.log('-------> response', response);
+      })
+  }, []);
+
   return (
     <div>task by id</div>
   );
