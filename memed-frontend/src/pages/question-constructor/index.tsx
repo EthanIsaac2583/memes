cf142a6bc91305074a5cd27a4f3d67cbbad5e57c
eitan.aizek <eitan.aizek@techork.com>
2023-11-23 11:25:55 +0500

    m: added question type selector

--- memed-frontend/src/pages/question-constructor/index.tsx
+++ memed-frontend/src/pages/question-constructor/index.tsx
@@ -2,17 +2,25 @@ import {BaseLayout} from "../../components/base-layout";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import {Form} from "react-bootstrap";
 
 export const QuestionConstructor = () => {
   return (
     <BaseLayout>
-      <Container>
-        <Row>
-          <Col>
-            <div>question constructor</div>
-          </Col>
-        </Row>
-      </Container>
+      <Form>
+        <Container>
+          <Row>
+            <Col md={4} xs={12}>
+                <Form.Select>
+                  <option>Open this select menu</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </Form.Select>
+            </Col>
+          </Row>
+        </Container>
+      </Form>
     </BaseLayout>
   );
 };
