d617571721e5f5f9700b2fff0144b8ed1892e581
eitan.aizek <eitan.aizek@techork.com>
2023-11-23 14:53:58 +0500

    m: exrtacted blank section

---
+++ memed-frontend/src/pages/question-constructor/blank-section.tsx
@@ -0,0 +1,61 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {CloseButton, Form} from "react-bootstrap";
+import {EBlankType} from "../../model/blank-type";
+import Button from "react-bootstrap/Button";
+import {FormSection} from "../../components/form-section";
+import {useFieldArray, useFormContext} from "react-hook-form";
+import Container from "react-bootstrap/Container";
+
+export const BlankSection = () => {
+  const { control, register } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    name: 'blank.options',
+    control
+  });
+
+  return (
+    <Container>
+      <FormSection>
+        <Row>
+          <Col>
+            <h3>Blank</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4} xs={12}>
+            <Form.Select {...register('blank.type')}>
+              <option value={EBlankType.SINGLE_CHOICE.toString()}>{EBlankType.SINGLE_CHOICE}</option>
+            </Form.Select>
+          </Col>
+        </Row>
+        {fields.map((field, i) => {
+          return (
+            <Row key={field.id} className='mt-3'>
+              <Col xs={3} md={3}>
+                <Form.Control placeholder="key" {...register(`blank.options.${i}.key`)} />
+              </Col>
+              <Col xs={7} md={7}>
+                <Form.Control placeholder="value" {...register(`blank.options.${i}.value`)} />
+              </Col>
+              <Col xs={2} md={2} className='d-flex justify-content-center align-items-center'>
+                <CloseButton type="button" onClick={() => remove(i)} />
+              </Col>
+            </Row>
+          )
+        })}
+        <Row className='mt-3'>
+          <Col>
+            <Button
+              variant="outline-dark"
+              type="button"
+              onClick={() => append({ key: '', value: '' })}
+            >
+              Add option
+            </Button>
+          </Col>
+        </Row>
+      </FormSection>
+    </Container>
+  );
+};
--- memed-frontend/src/pages/question-constructor/index.tsx
+++ memed-frontend/src/pages/question-constructor/index.tsx
@@ -2,20 +2,15 @@ import {BaseLayout} from "../../components/base-layout";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import {CloseButton, Form} from "react-bootstrap";
-import {EBlankType} from "../../model/blank-type";
+import {Form} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import axios, {AxiosError} from "axios";
 import {TTaskDto} from "../../dto/task";
-import {SubmitHandler, useFieldArray, useForm} from "react-hook-form";
-import {FormSection} from "../../components/form-section";
+import {SubmitHandler, useForm, FormProvider} from "react-hook-form";
+import {BlankSection} from "./blank-section";
 
 export const QuestionConstructor = () => {
-  const { handleSubmit, register, control } = useForm<TTaskDto>();
-  const { fields, append, remove } = useFieldArray({
-    name: 'blank.options',
-    control
-  })
+  const methods = useForm<TTaskDto>();
 
   const submit: SubmitHandler<TTaskDto> = (data) => {
     axios<TTaskDto, unknown>({
@@ -33,59 +28,20 @@ export const QuestionConstructor = () => {
 
   return (
     <BaseLayout>
-      <Form onSubmit={handleSubmit(submit)}>
-        <Container>
-          <FormSection>
-            <Row>
-              <Col>
-                <h3>Blank</h3>
+      <FormProvider {...methods}>
+        <Form onSubmit={methods.handleSubmit(submit)}>
+          <BlankSection />
+          <Container>
+            <Row className='justify-content-md-center mt-5'>
+              <Col xs={12} md={4}>
+                <div className="d-grid">
+                  <Button type="submit" size="lg">Submit</Button>
+                </div>
               </Col>
             </Row>
-            <Row>
-              <Col md={4} xs={12}>
-                <Form.Select {...register('blank.type')}>
-                  <option value={EBlankType.SINGLE_CHOICE.toString()}>{EBlankType.SINGLE_CHOICE}</option>
-                </Form.Select>
-              </Col>
-            </Row>
-            {fields.map((field, i) => {
-              return (
-                <Row key={field.id} className='mt-3'>
-                  <Col xs={3} md={3}>
-                    <Form.Control placeholder="key" {...register(`blank.options.${i}.key`)} />
-                  </Col>
-                  <Col xs={7} md={7}>
-                    <Form.Control placeholder="value" {...register(`blank.options.${i}.value`)} />
-                  </Col>
-                  <Col xs={2} md={2} className='d-flex justify-content-center align-items-center'>
-                    <CloseButton type="button" onClick={() => remove(i)} />
-                  </Col>
-                </Row>
-              )
-            })}
-            <Row className='mt-3'>
-              <Col>
-                <Button
-                  variant="outline-dark"
-                  type="button"
-                  onClick={() => append({ key: '', value: '' })}
-                >
-                  Add option
-                </Button>
-              </Col>
-            </Row>
-          </FormSection>
-        </Container>
-        <Container>
-          <Row className='justify-content-md-center mt-5'>
-            <Col xs={12} md={4}>
-              <div className="d-grid">
-                <Button type="submit" size="lg">Submit</Button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </Form>
+          </Container>
+        </Form>
+      </FormProvider>
     </BaseLayout>
   );
 };
