6c580c6838716d1e0b92c147c3c3786f87ae0353
eitan.aizek <eitan.aizek@techork.com>
2023-11-26 03:11:42 +0500

    m: added hide type field

--- memed-frontend/src/components/render-blank/single-choice-blank.tsx
+++ memed-frontend/src/components/render-blank/single-choice-blank.tsx
@@ -2,6 +2,7 @@ import {TSingleChoiceBlank} from "../../model/blank";
 import {FC} from "react";
 import { Form } from "react-bootstrap";
 import {useFormContext} from "react-hook-form";
+import {EBlankType} from "../../model/blank-type";
 
 interface IProps {
   blank: TSingleChoiceBlank;
@@ -14,6 +15,7 @@ export const SingleChoiceBlank: FC<IProps> = (props) => {
 
   return (
     <>
+      <input {...register('type')} defaultValue={EBlankType.SINGLE_CHOICE.toString()} hidden />
       {blank.options.map(option => {
         return (
           <Form.Check
