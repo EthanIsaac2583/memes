c3ca30b08a14ca117182929ddd76e3a816689c6d
eitan.aizek <eitan.aizek@techork.com>
2023-11-26 15:07:00 +0500

    m: managed task controller

--- memed-core/src/main/java/kz/ruanjian/memed/controller/TaskController.java
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/TaskController.java
@@ -4,9 +4,13 @@ import jakarta.validation.Valid;
 import kz.ruanjian.memed.dto.TaskDto;
 import kz.ruanjian.memed.model.Task;
 import kz.ruanjian.memed.service.TaskService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +30,27 @@ public class TaskController {
     return taskService.findById(id);
   }
 
+  @GetMapping
+  public Page<Task> findAll(Pageable pageable) {
+    return taskService.findAll(pageable);
+  }
+
   @PostMapping
   public void create(@RequestBody @Valid TaskDto task) {
     taskService.save(task);
+  }
+
+  @PutMapping("/{id}")
+  public void update(@PathVariable Long id,
+                     @RequestBody @Valid TaskDto task) {
+    taskService.findById(id);
+    task.setId(id);
+
+    taskService.save(task);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteById(@PathVariable Long id) {
+    taskService.deleteById(id);
   }
 }
