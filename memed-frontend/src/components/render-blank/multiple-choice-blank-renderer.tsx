e841fe926b54047e2f49b48dc6d3ae792a262455
eitan.aizek <eitan.aizek@techork.com>
2023-11-27 20:59:34 +0500

    m: small fixes

--- memed-frontend/src/components/render-blank/multiple-choice-blank-renderer.tsx
+++ memed-frontend/src/components/render-blank/multiple-choice-blank-renderer.tsx
@@ -3,25 +3,35 @@ import {FC} from "react";
 import {RectCheckbox} from "../ui-kit/rect-checkbox";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {BlankType} from "../../model/blank-type";
+import {useFormContext} from "react-hook-form";
 
 interface IProps {
     blank: MultipleChoiceBlank;
 }
 
 export const MultipleChoiceBlankRenderer: FC<IProps> = (props) => {
-    const { blank } = props;
+    const {blank} = props;
+
+    const {register} = useFormContext();
 
     return (
-        <div>
+        <>
+            <input {...register('type')} defaultValue={BlankType.MULTIPLE_CHOICE.toString()} hidden/>
             {blank.options.map(option => {
                 return (
-                    <Row key={option.key}>
+                    <Row key={option.key} className="mb-2">
                         <Col>
-                            <RectCheckbox />
+                            <RectCheckbox
+                                {...register('keys', {required: true})}
+                                id={option.key}
+                                value={option.key}
+                                label={option.value}
+                            />
                         </Col>
                     </Row>
                 );
             })}
-        </div>
+        </>
     );
 };
