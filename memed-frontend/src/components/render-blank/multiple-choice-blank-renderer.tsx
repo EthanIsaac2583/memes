7f95a430d332b831f4e1634863cbc5d17e1e270f
eitan.aizek <eitan.aizek@techork.com>
2023-11-27 20:47:32 +0500

    m: added RectCheckbox

--- memed-frontend/src/components/render-blank/multiple-choice-blank-renderer.tsx
+++ memed-frontend/src/components/render-blank/multiple-choice-blank-renderer.tsx
@@ -1,5 +1,8 @@
 import {MultipleChoiceBlank} from "../../model/blank";
 import {FC} from "react";
+import {RectCheckbox} from "../ui-kit/rect-checkbox";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 interface IProps {
     blank: MultipleChoiceBlank;
@@ -9,6 +12,16 @@ export const MultipleChoiceBlankRenderer: FC<IProps> = (props) => {
     const { blank } = props;
 
     return (
-        <div>MultipleChoiceBlankRenderer</div>
+        <div>
+            {blank.options.map(option => {
+                return (
+                    <Row key={option.key}>
+                        <Col>
+                            <RectCheckbox />
+                        </Col>
+                    </Row>
+                );
+            })}
+        </div>
     );
 };
---
+++ memed-frontend/src/components/ui-kit/rect-checkbox/index.tsx
@@ -0,0 +1,3 @@
+export const RectCheckbox = () => {
+    return <div>RectCheckbox</div>;
+};
