70a66dbcdbacb991e6e6e03d745d21c8b0a1e54d
eitan.aizek <eitan.aizek@techork.com>
2023-11-27 20:54:24 +0500

    m: styled RectCheckbox

--- memed-frontend/src/components/ui-kit/rect-checkbox/index.tsx
+++ memed-frontend/src/components/ui-kit/rect-checkbox/index.tsx
@@ -1,3 +1,26 @@
-export const RectCheckbox = () => {
-    return <div>RectCheckbox</div>;
-};
+import {FC, forwardRef} from "react";
+import './styles.scss';
+
+interface IProps {
+    id?: string;
+    value?: string;
+    label?: string;
+}
+
+export const RectCheckbox: FC<IProps> = forwardRef<HTMLInputElement, IProps>((props, ref) => {
+    const { id, label, value, ...otherProps } = props;
+
+    return (
+        <label className="rect-checkbox--root">
+            <input
+                {...otherProps}
+                ref={ref}
+                id={id}
+                type="checkbox"
+                value={value}
+                className="rect-checkbox--input"
+            />
+            <span className="rect-checkbox--text">{label}</span>
+        </label>
+    )
+});
