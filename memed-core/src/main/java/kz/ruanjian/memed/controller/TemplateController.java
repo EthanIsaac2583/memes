4adf1d5be19a5b87d0baae9cbf5cea2bfb64505f
eitan.aizek <eitan.aizek@techork.com>
2023-11-28 18:41:43 +0500

    m: added TemplateController

---
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/TemplateController.java
@@ -0,0 +1,32 @@
+package kz.ruanjian.memed.controller;
+
+import kz.ruanjian.memed.dto.TemplateDto;
+import kz.ruanjian.memed.model.Template;
+import kz.ruanjian.memed.service.TemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/templates")
+public class TemplateController {
+
+  private final TemplateService templateService;
+
+  public TemplateController(TemplateService templateService) {
+    this.templateService = templateService;
+  }
+
+  @GetMapping("/{id}")
+  public Template findById(@PathVariable Long id) {
+    return templateService.findById(id);
+  }
+
+  @PostMapping
+  public void create(@RequestBody TemplateDto template) {
+    templateService.save(template);
+  }
+}
