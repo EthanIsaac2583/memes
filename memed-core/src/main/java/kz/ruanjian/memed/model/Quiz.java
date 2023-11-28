2696aa5a7d6edb34fbe54a220ec028b13099807d
eitan.aizek <eitan.aizek@techork.com>
2023-11-29 03:06:58 +0500

    m: extended Quiz

--- memed-core/src/main/java/kz/ruanjian/memed/model/Quiz.java
+++ memed-core/src/main/java/kz/ruanjian/memed/model/Quiz.java
@@ -3,6 +3,8 @@ package kz.ruanjian.memed.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,11 +25,15 @@ public class Quiz {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Enumerated(EnumType.STRING)
+  private QuizStatus status;
+
   @JsonIgnore
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "template_id")
   private Template template;
 
+  @JsonIgnore
   @OneToMany(mappedBy = "quiz", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
   private Set<Question> questions;
 
@@ -40,6 +46,14 @@ public class Quiz {
 
   public void setId(Long id) {
     this.id = id;
+  }
+
+  public QuizStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(QuizStatus status) {
+    this.status = status;
   }
 
   public Template getTemplate() {
@@ -63,18 +77,19 @@ public class Quiz {
     if (this==o) return true;
     if (o==null || getClass()!=o.getClass()) return false;
     Quiz quiz = (Quiz) o;
-    return Objects.equals(id, quiz.id);
+    return Objects.equals(id, quiz.id) && status==quiz.status;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, status);
   }
 
   @Override
   public String toString() {
     return "Quiz{" +
       "id=" + id +
+      ", status=" + status +
       '}';
   }
 }
