90c59f22b45bd738ebd7eafd7e5b0a8a3e399e38
eitan.aizek <eitan.aizek@techork.com>
2023-11-28 18:59:58 +0500

    m: creating task

--- memed-core/src/main/java/kz/ruanjian/memed/service/TemplateService.java
+++ memed-core/src/main/java/kz/ruanjian/memed/service/TemplateService.java
@@ -1,11 +1,17 @@
 package kz.ruanjian.memed.service;
 
 import kz.ruanjian.memed.dto.TemplateDto;
+import kz.ruanjian.memed.mapper.TemplateMapper;
+import kz.ruanjian.memed.model.Task;
 import kz.ruanjian.memed.model.Template;
+import kz.ruanjian.memed.respository.TaskRepository;
 import kz.ruanjian.memed.respository.TemplateRepository;
 import kz.ruanjian.memed.service.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class TemplateService {
@@ -13,9 +19,15 @@ public class TemplateService {
   private static final String TEMPLATE_NOT_FOUND = "Template not found";
 
   private final TemplateRepository templateRepository;
+  private final TaskRepository taskRepository;
+  private final TemplateMapper templateMapper;
 
-  public TemplateService(TemplateRepository templateRepository) {
+  public TemplateService(TemplateRepository templateRepository,
+                         TaskRepository taskRepository,
+                         TemplateMapper templateMapper) {
     this.templateRepository = templateRepository;
+    this.taskRepository = taskRepository;
+    this.templateMapper = templateMapper;
   }
 
   public Template findById(Long id) {
@@ -25,11 +37,27 @@ public class TemplateService {
 
   @Transactional
   public Template save(TemplateDto templateDto) {
-    return new Template();
+    Template template = mapToTemplate(templateDto);
+    templateRepository.save(template);
+
+    return template;
   }
 
   @Transactional
   public void deleteById(Long id) {
     templateRepository.deleteById(id);
+  }
+
+  private Template mapToTemplate(TemplateDto templateDto) {
+    Template template = templateMapper.toTemplate(templateDto);
+
+    Set<Task> tasks = findTasksById(templateDto.getTaskIds());
+    template.setTasks(tasks);
+
+    return template;
+  }
+
+  private Set<Task> findTasksById(Set<Long> ids) {
+    return new HashSet<>(taskRepository.findAllById(ids));
   }
 }
