f355f72ecbb7e936aea7906a8574eb4750315e7a
eitan.aizek <eitan.aizek@techork.com>
2023-12-02 00:54:46 +0500

    m: base url from config

--- memed-frontend/src/repository/question-repository.ts
+++ memed-frontend/src/repository/question-repository.ts
@@ -1,17 +1,36 @@
 import {Question} from "../model/question";
 import axios, {AxiosError, AxiosResponse} from "axios";
 import {ErrorResponse} from "../model/error-response";
+import {Answer} from "../model/answer";
 
 export class QuestionRepository {
+
+  private readonly baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
 
   public async nextQuestion(quizId: number): Promise<Question> {
     return axios({
       method: 'GET',
-      url: `http://192.168.100.5:8080/api/v1/quizzes/${quizId}/questions/next`
+      url: `${this.baseUrl}/api/v1/quizzes/${quizId}/questions/next`
     })
       .then((response: AxiosResponse<Question>) => {
         return response.data;
       })
+      .catch((errorResponse: AxiosError<ErrorResponse>) => {
+        throw errorResponse?.response?.data;
+      });
+  }
+
+  public async provideAnswer(questionId: number , answer: Answer): Promise<void> {
+    return axios({
+      method: 'PATCH',
+      url: `${this.baseUrl}/api/v1/questions/${questionId}`,
+      data: { answer }
+    })
+      .then(() => {})
       .catch((errorResponse: AxiosError<ErrorResponse>) => {
         throw errorResponse?.response?.data;
       });
