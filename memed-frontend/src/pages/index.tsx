b6791ae943963ee5dee2377f3a08a812c0ef05e5
eitan.aizek <eitan.aizek@techork.com>
2023-12-01 23:40:35 +0500

    m: removed reduntant page

--- memed-frontend/src/pages/index.tsx
+++ memed-frontend/src/pages/index.tsx
@@ -2,13 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import {RootPage} from "./root";
 import {AboutPage} from "./about";
 import {TaskConstructor} from "./task-constructor";
-import {TaskById} from "./task-by-id";
 import {ProcessQuizByTemplateId} from "./process-quiz-by-template-id";
 
 export const router = createBrowserRouter([
   { path: '/', element: <RootPage /> },
   { path: '/about', element: <AboutPage /> },
   { path: '/task-constructor', element: <TaskConstructor /> },
-  { path: '/tasks/:id', element: <TaskById /> },
   { path: '/templates/:templateId/process-quiz', element: <ProcessQuizByTemplateId /> }
 ]);
