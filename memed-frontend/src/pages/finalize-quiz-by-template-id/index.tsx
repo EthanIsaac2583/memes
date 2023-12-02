1e13b7ca131fc89fff756294bdbf3639915cf1c1
eitan.aizek <eitan.aizek@techork.com>
2023-12-02 15:32:44 +0500

    m: get grade screen

---
+++ memed-frontend/src/pages/finalize-quiz-by-template-id/index.tsx
@@ -0,0 +1,54 @@
+import {useParams} from "react-router-dom";
+import {BaseLayout} from "../../components/base-layout";
+import {useRepositories} from "../../repository/repositories-context";
+import {useEffect, useState} from "react";
+import {Quiz} from "../../model/quiz";
+import {QuizStatus} from "../../model/quiz-status";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Alert} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+export const FinalizeQuizByTemplateId = () => {
+  const { templateId } = useParams();
+  const repositories = useRepositories();
+
+  const [quiz, setQuiz] = useState<Quiz | null>(null);
+
+  useEffect(() => {
+    if (templateId) {
+      repositories?.quizRepository
+        .requestByTemplateId(parseInt(templateId, 10))
+        .then(setQuiz);
+    }
+  }, [templateId]);
+
+  if (quiz === null) {
+    return null;
+  }
+
+  return (
+    <BaseLayout>
+      {quiz.status === QuizStatus.IN_PROGRESS ? (
+        <Container>
+          <Row>
+            <Col>
+              <Alert variant="success">
+                <Alert.Heading>Congratulations! <span className="h2">🎉🎉🎉</span></Alert.Heading>
+                You've completed our awesome quiz. Let's find out how many points you've got
+              </Alert>
+            </Col>
+          </Row>
+          <Row className="justify-content-center mt-5">
+            <Col md={4} className="d-grid">
+              <Button size="lg" type="button">Get grade</Button>
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <div>you</div>
+      )}
+    </BaseLayout>
+  );
+};
--- memed-frontend/src/pages/index.tsx
+++ memed-frontend/src/pages/index.tsx
@@ -3,10 +3,12 @@ import {RootPage} from "./root";
 import {AboutPage} from "./about";
 import {TaskConstructor} from "./task-constructor";
 import {ProcessQuizByTemplateId} from "./process-quiz-by-template-id";
+import {FinalizeQuizByTemplateId} from "./finalize-quiz-by-template-id";
 
 export const router = createBrowserRouter([
   { path: '/', element: <RootPage /> },
   { path: '/about', element: <AboutPage /> },
   { path: '/task-constructor', element: <TaskConstructor /> },
-  { path: '/templates/:templateId/process-quiz', element: <ProcessQuizByTemplateId /> }
+  { path: '/templates/:templateId/process-quiz', element: <ProcessQuizByTemplateId /> },
+  { path: '/templates/:templateId/finalize-quiz', element: <FinalizeQuizByTemplateId /> }
 ]);
