6731c7251abee3d65ded06e1b99b15c67942d2d9
eitan.aizek <eitan.aizek@techork.com>
2023-12-03 14:06:43 +0500

    m: used MemedProperties in SingleChoiceAnswerGrader

--- memed-core/src/main/java/kz/ruanjian/memed/util/grader/SingleChoiceAnswerGrader.java
+++ memed-core/src/main/java/kz/ruanjian/memed/util/grader/SingleChoiceAnswerGrader.java
@@ -1,13 +1,32 @@
 package kz.ruanjian.memed.util.grader;
 
+import kz.ruanjian.memed.config.MemedProperties;
 import kz.ruanjian.memed.model.Question;
+import kz.ruanjian.memed.pojo.answer.Answer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SingleChoiceAnswerGrader implements Grader {
 
+  private final MemedProperties memedProperties;
+
+  public SingleChoiceAnswerGrader(MemedProperties memedProperties) {
+    this.memedProperties = memedProperties;
+  }
+
   @Override
   public int grade(Question question) {
-    return 0;
+    if (isCorrectAnswer(question)) {
+      return memedProperties.getGradeMax();
+    }
+
+    return memedProperties.getGradeMin();
+  }
+
+  private boolean isCorrectAnswer(Question question) {
+    Answer correctAnswer = question.getTask().getAnswer();
+    Answer userAnswer = question.getAnswer();
+
+    return correctAnswer.equals(userAnswer);
   }
 }
