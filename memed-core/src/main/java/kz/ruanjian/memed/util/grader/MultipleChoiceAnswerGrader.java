b675b8610c7403667b8a39251dc12f9d06d91dfb
eitan.aizek <eitan.aizek@techork.com>
2023-12-03 14:04:53 +0500

    m: grading single choice answer

--- memed-core/src/main/java/kz/ruanjian/memed/util/grader/MultipleChoiceAnswerGrader.java
+++ memed-core/src/main/java/kz/ruanjian/memed/util/grader/MultipleChoiceAnswerGrader.java
@@ -1,6 +1,7 @@
 package kz.ruanjian.memed.util.grader;
 
 import kz.ruanjian.memed.model.Question;
+import kz.ruanjian.memed.pojo.answer.Answer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,17 @@ public class MultipleChoiceAnswerGrader implements Grader {
 
   @Override
   public int grade(Question question) {
+    if (isCorrectAnswer(question)) {
+      return 100;
+    }
+
     return 0;
+  }
+
+  private boolean isCorrectAnswer(Question question) {
+    Answer correctAnswer = question.getTask().getAnswer();
+    Answer userAnswer = question.getAnswer();
+
+    return correctAnswer.equals(userAnswer);
   }
 }
