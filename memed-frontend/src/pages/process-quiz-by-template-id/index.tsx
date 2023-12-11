12a4055fbbf57c87de1133ebc168c5c087597c7e
eitan.aizek <eitan.aizek@techork.com>
2023-12-11 21:13:58 +0500

    m: FE template process

--- memed-frontend/src/pages/process-quiz-by-template-id/index.tsx
+++ memed-frontend/src/pages/process-quiz-by-template-id/index.tsx
@@ -1,39 +1,19 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {Quiz} from "../../model/quiz";
-import {ProcessQuiz} from "../../components/process-quiz";
-import {BaseLayout} from "../../components/base-layout";
+import {useEffect} from "react";
 import {useRepositories} from "../../repository/repositories-context";
 
 export const ProcessQuizByTemplateId = () => {
-  const { templateId } = useParams();
+  const { templateId = '' } = useParams();
   const navigate = useNavigate();
   const repositories = useRepositories();
 
-  const [quiz, setQuiz] = useState<Quiz | null>(null);
-
   useEffect(() => {
-    if (templateId) {
-      repositories?.quizRepository
-        .requestByTemplateId(parseInt(templateId, 10))
-        .then(setQuiz);
-    }
-  }, [templateId]);
+    repositories?.quizRepository
+      .requestByTemplateId(parseInt(templateId, 10))
+      .then(fetchedQuiz => {
+        navigate(`/questions/item?quizId=${fetchedQuiz.id}`);
+      });
+  }, []);
 
-  const handleEndQuiz = (endedQuiz: Quiz) => {
-    navigate(`/quizzes/${endedQuiz.id}/finalize`);
-  };
-
-  if (quiz === null) {
-    return null;
-  }
-
-  return (
-    <BaseLayout>
-      <ProcessQuiz
-        quiz={quiz}
-        onEnd={handleEndQuiz}
-      />
-    </BaseLayout>
-  );
+  return null;
 };
