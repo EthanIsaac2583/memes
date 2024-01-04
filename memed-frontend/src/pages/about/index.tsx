2569a35b8ec4dff04635a09cb81d117733e62fdf
eitan.aizek <eitan.aizek@techork.com>
2024-01-05 00:28:19 +0500

    m: added about me text

--- memed-frontend/src/pages/about/index.tsx
+++ memed-frontend/src/pages/about/index.tsx
@@ -4,7 +4,10 @@ export const AboutPage = () => {
   return (
     <BaseLayout>
       <div>
-        Hello! This is about page
+        <h3>Hi!</h3>
+        <p>I am Yerassyl Aitkazy, a web developer based in Kazakhstan. Currently serving as a Frontend Developer at KIVORK. All the way actively developed B2B and C2B system solutions.</p>
+        <p>At the moment, I am deeply inspired by backend development and am aligning my future career growth with it. My previous career focus was on frontend development (React/JS/TS).</p>
+        <p> Connect with me at millennial.93@yandex.kz or visit LinkedIn profile at www.linkedin.com/in/yerassyl-aitkazy-99205b205 for more insights into his professional journey.</p>
       </div>
     </BaseLayout>
   );
