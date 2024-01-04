1d052e3a955141d2779b5619281c1e2d93da9d36
eitan.aizek <eitan.aizek@techork.com>
2024-01-04 23:50:35 +0500

    m: fixed header link

--- memed-frontend/src/components/base-layout/index.tsx
+++ memed-frontend/src/components/base-layout/index.tsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/logo.svg";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {Link} from "react-router-dom";
 
 export const BaseLayout: FC<PropsWithChildren> = (props) => {
   return (
@@ -15,10 +16,10 @@ export const BaseLayout: FC<PropsWithChildren> = (props) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
-              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/">Quizzes</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="/task-constructor">Task Constructor</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
