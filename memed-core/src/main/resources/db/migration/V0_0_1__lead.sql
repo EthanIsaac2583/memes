074957a05d74c8222f7b88cde70f3f67834456c2
eitan.aizek <eitan.aizek@techork.com>
2024-01-10 01:25:29 +0500

    m: reformatted sql

--- memed-core/src/main/resources/db/migration/V0_0_1__lead.sql
+++ memed-core/src/main/resources/db/migration/V0_0_1__lead.sql
@@ -1,13 +1,13 @@
 create table visits
 (
-  id uuid primary key,
+  id         uuid primary key,
   user_agent varchar,
   created_at timestamptz not null
 );
 
 create table leads
 (
-  id   bigserial primary key,
-  name varchar(150),
+  id       bigserial primary key,
+  name     varchar(150),
   visit_id uuid references visits (id) not null
 );
