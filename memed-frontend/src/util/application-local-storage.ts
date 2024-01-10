aa4cb1b89d5c002477bde2e77523de1f61155972
eitan.aizek <eitan.aizek@techork.com>
2024-01-10 23:20:28 +0500

    m: added ApplicationLocalStorage

---
+++ memed-frontend/src/util/application-local-storage.ts
@@ -0,0 +1,23 @@
+export class ApplicationLocalStorage {
+
+  private static readonly PREFIX = 'memed_app__'
+
+  public static getItem(key: string): string | null {
+    const fullKey = ApplicationLocalStorage.buildItemFullKey(key);
+    return window.localStorage.getItem(fullKey);
+  }
+
+  public static setItem(key: string, value: string): void {
+    const fullKey = ApplicationLocalStorage.buildItemFullKey(key);
+    window.localStorage.setItem(fullKey, value);
+  }
+
+  public static removeItem(key: string): void {
+    const fullKey = ApplicationLocalStorage.buildItemFullKey(key);
+    window.localStorage.removeItem(fullKey);
+  }
+
+  private static buildItemFullKey(name: string) {
+    return ApplicationLocalStorage.PREFIX + name;
+  }
+}
