6cf32228bfe7301ddade57cddac52cffaefff484
eitan.aizek <eitan.aizek@techork.com>
2024-01-10 23:36:10 +0500

    m: visit requested

--- memed-frontend/src/components/visit-requester/index.tsx
+++ memed-frontend/src/components/visit-requester/index.tsx
@@ -1,15 +1,28 @@
 import {FC, PropsWithChildren, useEffect, useState} from "react";
 import {ApplicationLocalStorage, StorageKey} from "../../util/application-local-storage";
+import {useRepositories} from "../../repository/repositories-context";
 
 export const VisitRequester: FC<PropsWithChildren> = (props) => {
   const { children } = props;
+
   const [fetching, setFetching] = useState(false);
+
+  const repositories = useRepositories();
 
   useEffect(() => {
     const visitId = ApplicationLocalStorage.getItem(StorageKey.VisitId);
 
     if (!visitId) {
       setFetching(true);
+      repositories?.visitRepository
+        .create()
+        .then(visit => {
+          ApplicationLocalStorage.setItem(StorageKey.VisitId, visit.id);
+          setFetching(false);
+        })
+        .catch(() => {
+          setFetching(false);
+        });
     }
   }, []);
 
