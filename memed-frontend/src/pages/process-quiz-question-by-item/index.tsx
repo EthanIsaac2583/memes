4762c9e2e3425d374cd68abf0cdaf48652de2646
eitan.aizek <eitan.aizek@techork.com>
2024-01-11 02:57:24 +0500

    m: forbidden to read

--- memed-frontend/src/pages/process-quiz-question-by-item/index.tsx
+++ memed-frontend/src/pages/process-quiz-question-by-item/index.tsx
@@ -6,6 +6,10 @@ import {Item} from "../../model/item";
 import {Question} from "../../model/question";
 import {ErrorResponse} from "../../model/error-response";
 import {BaseLayout} from "../../components/base-layout";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Alert} from "react-bootstrap";
 
 export const ProcessQuestionItemByQuizId = () => {
   const { quizId = '' } = useParams();
@@ -13,14 +17,17 @@ export const ProcessQuestionItemByQuizId = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const repositories = useRepositories();
   const [questionItem, setQuestionItem] = useState<Item<Question> | null>(null);
+  const [error, setError] = useState<ErrorResponse | undefined>();
 
   useEffect(() => {
     repositories?.questionRepository
       .nextQuestion(quizId, searchParams.get('number'))
       .then(setQuestionItem)
-      .catch((error: ErrorResponse) => {
-        if (error.statusCode === 404) {
+      .catch((errorResponse: ErrorResponse) => {
+        if (errorResponse.statusCode === 404) {
           navigate(`/quizzes/${quizId}/finalize`);
+        } else {
+          setError(errorResponse);
         }
       });
   }, [repositories, searchParams]);
@@ -34,18 +41,24 @@ export const ProcessQuestionItemByQuizId = () => {
     navigate(`/quizzes/${quizId}/finalize`);
   };
 
-  if (questionItem === null) {
-    return null;
-  }
-
   return (
     <BaseLayout>
-      <ProcessQuestion
-        quizId={quizId}
-        questionItem={questionItem}
-        onNavigate={handleNavigate}
-        onProcessed={handleProcessed}
-      />
+      {error && (
+        <Container>
+          <Row>
+            <Col>{error && (<Alert variant="danger">{error.message}</Alert>)}</Col>
+          </Row>
+        </Container>
+      )}
+      {questionItem && (
+        <ProcessQuestion
+          quizId={quizId}
+          questionItem={questionItem}
+          onNavigate={handleNavigate}
+          onProcessed={handleProcessed}
+        />
+      )}
+
     </BaseLayout>
   );
 }
