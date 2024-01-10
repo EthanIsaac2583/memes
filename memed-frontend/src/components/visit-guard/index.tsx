09118d393da53a97ef60f8ae73c1fe134a3220a0
eitan.aizek <eitan.aizek@techork.com>
2024-01-11 02:11:08 +0500

    m: request visit id on guard

--- memed-frontend/src/components/visit-guard/index.tsx
+++ memed-frontend/src/components/visit-guard/index.tsx
@@ -1,11 +1,15 @@
-import {FC, PropsWithChildren, useEffect} from "react";
+import {FC, PropsWithChildren, useMemo} from "react";
+import {ApplicationLocalStorage, StorageKey} from "../../util/application-local-storage";
+import {VisitRequester} from "../visit-requester";
 
 export const VisitGuard: FC<PropsWithChildren> = (props) => {
   const { children } = props;
 
-  useEffect(() => {
+  const visitId = useMemo(() => ApplicationLocalStorage.getItem(StorageKey.VisitId), []);
 
-  }, []);
+  if (!visitId) {
+    return <VisitRequester />;
+  }
 
   return <>{children}</>
 }
--- memed-frontend/src/pages/process-quiz-by-template-id/index.tsx
+++ memed-frontend/src/pages/process-quiz-by-template-id/index.tsx
@@ -1,20 +1,37 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRepositories} from "../../repository/repositories-context";
 import {BaseLayout} from "../../components/base-layout";
+import {ErrorResponse} from "../../model/error-response";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {Alert} from "react-bootstrap";
 
 export const ProcessQuizByTemplateId = () => {
   const { templateId = '' } = useParams();
   const navigate = useNavigate();
   const repositories = useRepositories();
+  const [error, setError] = useState<ErrorResponse | undefined>();
 
   useEffect(() => {
     repositories?.quizRepository
       .requestByTemplateId(parseInt(templateId, 10))
       .then(fetchedQuiz => {
         navigate(`/quizzes/${fetchedQuiz.id}/questions/item`);
-      });
+      })
+      .catch(setError);
   }, []);
 
-  return <BaseLayout />;
+  return (
+    <BaseLayout>
+      <Container>
+        <Row>
+          <Col>
+            {error && (<Alert variant="danger">{error.message}</Alert>)}
+          </Col>
+        </Row>
+      </Container>
+    </BaseLayout>
+  );
 };
