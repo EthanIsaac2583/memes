0f330aff3527fa2c46222d8efbd035cb1e76aee6
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 18:02:43 +0500

    m: added jjwt deps

---
+++ memed-core/src/main/java/kz/ruanjian/memed/security/JwtService.java
@@ -0,0 +1,11 @@
+package kz.ruanjian.memed.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtService {
+
+  public String extractUsername(String token) {
+
+  }
+}
