ecb7a5b4ce6b3f66626bb058239d7a94fa256cbb
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 19:03:12 +0500

    m: added beans of atuh

--- memed-core/src/main/java/kz/ruanjian/memed/config/ApplicationConfig.java
+++ memed-core/src/main/java/kz/ruanjian/memed/config/ApplicationConfig.java
@@ -3,16 +3,43 @@ package kz.ruanjian.memed.config;
 import kz.ruanjian.memed.respository.LeadRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
   @Bean
   public UserDetailsService userDetailsService(LeadRepository leadRepository) {
     return username -> leadRepository
       .findByUsername(username)
       .orElseThrow(() -> new UsernameNotFoundException("Lead not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+                                                       PasswordEncoder passwordEncoder) {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(passwordEncoder);
+
+    return provider;
   }
 }
