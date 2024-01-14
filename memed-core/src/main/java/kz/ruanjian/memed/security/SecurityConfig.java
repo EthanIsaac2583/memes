dbd2c87182429aa2c05313e762f19df2b2283994
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 18:59:51 +0500

    m: added SecurityConfig

---
+++ memed-core/src/main/java/kz/ruanjian/memed/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package kz.ruanjian.memed.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final AuthenticationProvider authenticationProvider;
+
+  public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
+                        AuthenticationProvider authenticationProvider) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    this.authenticationProvider = authenticationProvider;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+      .csrf(AbstractHttpConfigurer::disable);
+
+    httpSecurity
+      .authorizeHttpRequests(request -> request
+        .requestMatchers("/api/v1/test/**").permitAll()
+        .anyRequest().authenticated());
+
+    httpSecurity
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    httpSecurity
+      .authenticationProvider(authenticationProvider);
+
+    httpSecurity
+      .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return httpSecurity.build();
+  }
+}
