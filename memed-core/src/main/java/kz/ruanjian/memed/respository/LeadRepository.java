823127667b62e510c215717ec11592c410f17160
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 17:43:46 +0500

    m: added LeadRepository.findByUsername()

--- memed-core/src/main/java/kz/ruanjian/memed/respository/LeadRepository.java
+++ memed-core/src/main/java/kz/ruanjian/memed/respository/LeadRepository.java
@@ -4,6 +4,10 @@ import kz.ruanjian.memed.model.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LeadRepository extends JpaRepository<Lead, Long> {
+
+  Optional<Lead> findByUsername(String username);
 }
