ac8c6740443f7948e6943b097e18028584cba865
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 19:26:34 +0500

    m: added AuthService.login()

--- memed-core/src/main/java/kz/ruanjian/memed/service/AuthService.java
+++ memed-core/src/main/java/kz/ruanjian/memed/service/AuthService.java
@@ -8,6 +8,9 @@ import kz.ruanjian.memed.model.Visit;
 import kz.ruanjian.memed.respository.LeadRepository;
 import kz.ruanjian.memed.respository.VisitRepository;
 import kz.ruanjian.memed.security.JwtService;
+import kz.ruanjian.memed.service.exception.NotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,19 +20,22 @@ import java.time.ZonedDateTime;
 @Service
 public class AuthService {
 
-  private LeadRepository leadRepository;
-  private VisitRepository visitRepository;
-  private PasswordEncoder passwordEncoder;
-  private JwtService jwtService;
+  private final LeadRepository leadRepository;
+  private final VisitRepository visitRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
 
   public AuthService(LeadRepository leadRepository,
                      VisitRepository visitRepository,
                      PasswordEncoder passwordEncoder,
-                     JwtService jwtService) {
+                     JwtService jwtService,
+                     AuthenticationManager authenticationManager) {
     this.leadRepository = leadRepository;
     this.visitRepository = visitRepository;
     this.passwordEncoder = passwordEncoder;
     this.jwtService = jwtService;
+    this.authenticationManager = authenticationManager;
   }
 
   @Transactional
@@ -46,8 +52,21 @@ public class AuthService {
     return authDto;
   }
 
+  @Transactional
   public AuthDto login(LoginDto loginDto) {
-    return new AuthDto();
+    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+    Lead lead = findLeadByUsername(loginDto.getUsername());
+    String token = jwtService.generateToken(lead);
+
+    AuthDto authDto = new AuthDto();
+    authDto.setToken(token);
+    return authDto;
+  }
+
+  private Lead findLeadByUsername(String username) {
+    return leadRepository
+      .findByUsername(username)
+      .orElseThrow(() -> new NotFoundException("Lead not found"));
   }
 
   private Visit createVisit() {
