cf07803128ed80d81118072df9d0b3c0eafdfc1e
eitan.aizek <eitan.aizek@techork.com>
2024-01-14 17:21:06 +0500

    m: added Lead

--- memed-core/src/main/java/kz/ruanjian/memed/model/Lead.java
+++ memed-core/src/main/java/kz/ruanjian/memed/model/Lead.java
@@ -3,7 +3,11 @@ package kz.ruanjian.memed.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "leads")
@@ -13,5 +17,56 @@ public class Lead {
   @GeneratedValue
   private Long id;
 
-  private
+  private String username;
+
+  private String password;
+
+  @OneToOne
+  @JoinColumn(name = "visit_id")
+  private Visit visit;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Visit getVisit() {
+    return visit;
+  }
+
+  public void setVisit(Visit visit) {
+    this.visit = visit;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    Lead lead = (Lead) o;
+    return Objects.equals(id, lead.id) && Objects.equals(username, lead.username) && Objects.equals(password, lead.password);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, password);
+  }
 }
