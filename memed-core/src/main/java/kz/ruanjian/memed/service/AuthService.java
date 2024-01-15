e0891b277b019f6e8a64364668cc00aec9f39fba
eitan.aizek <eitan.aizek@techork.com>
2024-01-15 21:31:04 +0500

    m: added register verification

--- memed-core/src/main/java/kz/ruanjian/memed/service/AuthService.java
+++ memed-core/src/main/java/kz/ruanjian/memed/service/AuthService.java
@@ -8,6 +8,7 @@ import kz.ruanjian.memed.model.Visit;
 import kz.ruanjian.memed.respository.LeadRepository;
 import kz.ruanjian.memed.respository.VisitRepository;
 import kz.ruanjian.memed.security.JwtManager;
+import kz.ruanjian.memed.service.exception.DataConflictException;
 import kz.ruanjian.memed.service.exception.NotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,9 +17,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Service
 public class AuthService {
+
+  private static final String NOT_FOUND = "Lead not found";
 
   private final LeadRepository leadRepository;
   private final VisitRepository visitRepository;
@@ -38,35 +42,28 @@ public class AuthService {
     this.authenticationManager = authenticationManager;
   }
 
+  public Lead findLeadByUsername(String username) {
+    return leadRepository
+      .findByUsername(username)
+      .orElseThrow(() -> new NotFoundException(NOT_FOUND));
+  }
+
   @Transactional
   public AuthDto register(RegisterDto registerDto) {
     Visit visit = createVisit();
     Lead lead = generateLead(registerDto, visit);
+    verify(lead);
     leadRepository.save(lead);
 
-    String token = jwtManager.generateToken(lead);
-
-    AuthDto authDto = new AuthDto();
-    authDto.setToken(token);
-
-    return authDto;
+    return generateAuthDto(lead);
   }
 
   @Transactional
   public AuthDto login(LoginDto loginDto) {
     authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
     Lead lead = findLeadByUsername(loginDto.getUsername());
-    String token = jwtManager.generateToken(lead);
 
-    AuthDto authDto = new AuthDto();
-    authDto.setToken(token);
-    return authDto;
-  }
-
-  private Lead findLeadByUsername(String username) {
-    return leadRepository
-      .findByUsername(username)
-      .orElseThrow(() -> new NotFoundException("Lead not found"));
+    return generateAuthDto(lead);
   }
 
   private Visit createVisit() {
@@ -74,6 +71,14 @@ public class AuthService {
     visit.setCreatedAt(ZonedDateTime.now());
 
     return visitRepository.save(visit);
+  }
+
+  private AuthDto generateAuthDto(Lead lead) {
+    String token = jwtManager.generateToken(lead);
+    AuthDto authDto = new AuthDto();
+    authDto.setToken(token);
+
+    return authDto;
   }
 
   private Lead generateLead(RegisterDto registerDto, Visit visit) {
@@ -84,5 +89,23 @@ public class AuthService {
     lead.setVisit(visit);
 
     return lead;
+  }
+
+  private void verify(Lead lead) {
+    verifyUsernameIsUnique(lead);
+  }
+
+  private void verifyUsernameIsUnique(Lead lead) {
+    Optional<Lead> persistedLead = leadRepository.findByUsername(lead.getUsername());
+
+    if (!isEqualById(lead, persistedLead)) {
+      throw new DataConflictException("Username already exists");
+    }
+  }
+
+  private boolean isEqualById(Lead lead, Optional<Lead> persistedLead) {
+      return persistedLead
+        .map(value -> value.getId().equals(lead.getId()))
+        .orElse(false);
   }
 }
