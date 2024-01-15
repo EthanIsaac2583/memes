bcd973ee337e628f2dde0cdc15ecdf66e15d7804
eitan.aizek <eitan.aizek@techork.com>
2024-01-15 20:41:34 +0500

    m: cors configuration

--- memed-core/src/main/java/kz/ruanjian/memed/security/SecurityConfig.java
+++ memed-core/src/main/java/kz/ruanjian/memed/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package kz.ruanjian.memed.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +32,9 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
+      .cors(cors -> cors.configurationSource(this::generateCorsConfiguration));
+
+    httpSecurity
       .csrf(AbstractHttpConfigurer::disable);
 
     httpSecurity
@@ -45,5 +52,16 @@ public class SecurityConfig {
       .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
+  }
+
+  private CorsConfiguration generateCorsConfiguration(HttpServletRequest request) {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOriginPatterns(List.of("*"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    return configuration;
   }
 }
