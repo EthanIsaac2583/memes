30bd30de501b3de28b718c2863ab8fe8cc53ce72
eitan.aizek <eitan.aizek@techork.com>
2024-01-17 19:42:41 +0500

    m: added VisitController.findById()

--- memed-core/src/main/java/kz/ruanjian/memed/controller/VisitController.java
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/VisitController.java
@@ -3,10 +3,14 @@ package kz.ruanjian.memed.controller;
 import kz.ruanjian.memed.dto.VisitDto;
 import kz.ruanjian.memed.model.Visit;
 import kz.ruanjian.memed.service.VisitService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/visits")
@@ -16,6 +20,11 @@ public class VisitController {
 
   public VisitController(VisitService visitService) {
     this.visitService = visitService;
+  }
+
+  @GetMapping("/${id}")
+  public Visit findById(@PathVariable UUID id) {
+    return visitService.findById(id);
   }
 
   @PostMapping
