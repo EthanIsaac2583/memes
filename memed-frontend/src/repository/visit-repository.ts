ac485987a4fbf3d29d52912f705132c01747e99d
eitan.aizek <eitan.aizek@techork.com>
2024-01-18 22:16:34 +0500

    m: added VisitRepository.findById()

--- memed-frontend/src/repository/visit-repository.ts
+++ memed-frontend/src/repository/visit-repository.ts
@@ -3,10 +3,25 @@ import {ErrorResponse} from "../model/error-response";
 import {Visit} from "../model/visit";
 
 export class VisitRepository {
+
   private readonly baseUrl: string;
 
   constructor(baseUrl: string) {
     this.baseUrl = baseUrl;
+  }
+
+  public async findById(id: string) {
+    return axios({
+      method: 'POST',
+      baseURL: this.baseUrl,
+      url: `/api/v1/visits/${id}`
+    })
+      .then((response: AxiosResponse<Visit>) => {
+        return response.data;
+      })
+      .catch((errorResponse: AxiosError<ErrorResponse>) => {
+        throw errorResponse?.response?.data;
+      });
   }
 
   public async create() {
