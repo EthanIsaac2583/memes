1e10a7a3790d44790e431c5bfa88c623fc4b5df2
eitan.aizek <eitan.aizek@techork.com>
2024-01-18 20:29:12 +0500

    m: fe keep changes

--- memed-frontend/src/pages/history/index.tsx
+++ memed-frontend/src/pages/history/index.tsx
@@ -1,6 +1,13 @@
 import {BaseLayout} from "../../components/base-layout";
+import {useState} from "react";
+import {Page} from "../../model/page";
+import {Quiz} from "../../model/quiz";
 
 export const HistoryPage = () => {
+  const [quizPage, setQuizPage] = useState<Page<Quiz> | null>(null);
+
+
+
   return (
     <BaseLayout>
       <div>history</div>
