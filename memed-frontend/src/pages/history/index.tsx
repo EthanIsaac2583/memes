f8fe1ea88b1cb8bf05eaecf64b496c91f63fa9c4
eitan.aizek <eitan.aizek@techork.com>
2024-01-18 21:11:06 +0500

    m: token expiration handling

--- memed-frontend/src/pages/history/index.tsx
+++ memed-frontend/src/pages/history/index.tsx
@@ -1,21 +1,31 @@
 import {BaseLayout} from "../../components/base-layout";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {Page} from "../../model/page";
 import {Quiz} from "../../model/quiz";
 import {useRepositories} from "../../repository/repositories-context";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import {ErrorResponse} from "../../model/error-response";
+import {useNavigate} from "react-router-dom";
+import {ApplicationLocalStorage, StorageKey} from "../../util/application-local-storage";
+import {authContext} from "../../components/auth";
 
 export const HistoryPage = () => {
   const [quizPage, setQuizPage] = useState<Page<Quiz> | null>(null);
   const repositories = useRepositories();
+  const navigate = useNavigate();
+  const authManager = useContext(authContext);
 
   useEffect(() => {
     repositories?.quizRepository
       .findAll()
       .then(setQuizPage)
-      .catch((error) => {
-        console.log('-------> error', error);
+      .catch((error: ErrorResponse) => {
+        if (error.statusCode === 403) {
+          ApplicationLocalStorage.removeItem(StorageKey.Token);
+          authManager.setLead(null);
+          navigate("/");
+        }
       });
   }, []);
 
