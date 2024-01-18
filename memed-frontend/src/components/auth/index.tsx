3ac692b50a6c05adcad390f45e7a5be4ccf3121c
eitan.aizek <eitan.aizek@techork.com>
2024-01-18 15:29:33 +0500

    m: fixed AuthProvider

--- memed-frontend/src/components/auth/index.tsx
+++ memed-frontend/src/components/auth/index.tsx
@@ -1,13 +1,25 @@
-import {createContext, FC, PropsWithChildren, useState} from 'react';
+import {createContext, FC, PropsWithChildren, useMemo, useState} from 'react';
 import {Lead} from "../../model/lead";
 
 type AuthLead = Lead | null;
 
-export const authContext = createContext<AuthLead>(null);
+type AuthManager = {
+  lead: AuthLead;
+  setLead: (lead: AuthLead) => void;
+};
+
+const DEFAULT_AUTH_MANAGER: AuthManager = {
+  lead: null,
+  setLead: () => {}
+};
+
+export const authContext = createContext<AuthManager>(DEFAULT_AUTH_MANAGER);
 
 export const AuthProvider: FC<PropsWithChildren> = (props) => {
   const { children } = props;
   const [lead, setLead] = useState<AuthLead>(null);
 
-  return <authContext.Provider value={lead}>{children}</authContext.Provider>;
+  const manager = useMemo(() => ({ lead, setLead }), [lead, setLead]);
+
+  return <authContext.Provider value={manager}>{children}</authContext.Provider>;
 };
