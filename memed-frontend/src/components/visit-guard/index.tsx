f98bfb72ca41047e699d962cc917858cdea41e84
eitan.aizek <eitan.aizek@techork.com>
2024-01-18 22:27:57 +0500

    m: visit guard fix

--- memed-frontend/src/components/visit-guard/index.tsx
+++ memed-frontend/src/components/visit-guard/index.tsx
@@ -1,20 +1,47 @@
-import {FC, PropsWithChildren, useMemo} from "react";
-import {ApplicationLocalStorage, StorageKey} from "../../util/application-local-storage";
-import {VisitRequester} from "../visit-requester";
+import {FC, PropsWithChildren, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {ApplicationLocalStorage, StorageKey} from "../../util/application-local-storage";
+import {useRepositories} from "../../repository/repositories-context";
 
 export const VisitGuard: FC<PropsWithChildren> = (props) => {
   const { children } = props;
   const navigate = useNavigate();
-
-  const visitId = useMemo(() => ApplicationLocalStorage.getItem(StorageKey.VisitId), []);
+  const repositories = useRepositories();
+  const [verifying, setVerifying] = useState(true);
 
   const handleNavigateToRoot = () => {
     navigate("/");
   };
 
-  if (!visitId) {
-    return <VisitRequester onRequested={handleNavigateToRoot} />;
+  useEffect(() => {
+    const handler = async () => {
+      if (repositories) {
+        const visitId = ApplicationLocalStorage.getItem(StorageKey.VisitId);
+
+        if (visitId) {
+          try {
+            const visit = await repositories.visitRepository.findById(visitId);
+            ApplicationLocalStorage.setItem(StorageKey.VisitId, visit.id);
+          } catch (e) {
+            ApplicationLocalStorage.removeItem(StorageKey.VisitId);
+          }
+        }
+
+        if (!visitId) {
+          const visit = await repositories.visitRepository.create();
+          ApplicationLocalStorage.setItem(StorageKey.VisitId, visit.id);
+        }
+      }
+    };
+
+    handler().then(() => {
+      setVerifying(false);
+      handleNavigateToRoot();
+    });
+  }, [repositories]);
+
+  if (verifying) {
+    return null;
   }
 
   return <>{children}</>
