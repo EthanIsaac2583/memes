ee8545462738ae19008fa0b349f6916de2c7be2f
eitan.aizek <eitan.aizek@techork.com>
2024-01-19 23:08:46 +0500

    m: added logger

--- memed-core/src/main/java/kz/ruanjian/memed/controller/TemplateController.java
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/TemplateController.java
@@ -3,6 +3,8 @@ package kz.ruanjian.memed.controller;
 import kz.ruanjian.memed.dto.TemplateDto;
 import kz.ruanjian.memed.model.Template;
 import kz.ruanjian.memed.service.TemplateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/templates")
 public class TemplateController {
+
+  private Logger log = LoggerFactory.getLogger(TemplateController.class);
 
   private final TemplateService templateService;
 
@@ -29,6 +33,9 @@ public class TemplateController {
 
   @GetMapping
   public Page<Template> findAll(Pageable pageable) {
+    if (log.isDebugEnabled()) {
+      log.debug("-------> You are logging your app!!!");
+    }
     return templateService.findAll(pageable);
   }
 
