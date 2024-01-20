af8992732c52589986496172b393609af7d1c52d
eitan.aizek <eitan.aizek@techork.com>
2024-01-21 02:22:22 +0500

    m: added Builder to PlainTextBody

--- memed-core/src/main/java/kz/ruanjian/memed/pojo/body/PlainTextBody.java
+++ memed-core/src/main/java/kz/ruanjian/memed/pojo/body/PlainTextBody.java
@@ -1,7 +1,38 @@
 package kz.ruanjian.memed.pojo.body;
 
+import kz.ruanjian.memed.pojo.BodyType;
+
 public class PlainTextBody extends Body {
 
   public PlainTextBody() {
+  }
+
+  private PlainTextBody(Builder builder) {
+    type = builder.type;
+    text = builder.text;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+
+    private BodyType type;
+    private String text;
+
+    public Builder type(BodyType type) {
+      this.type = type;
+      return this;
+    }
+
+    public Builder text(String text) {
+      this.text = text;
+      return this;
+    }
+
+    public PlainTextBody build() {
+      return new PlainTextBody(this);
+    }
   }
 }
