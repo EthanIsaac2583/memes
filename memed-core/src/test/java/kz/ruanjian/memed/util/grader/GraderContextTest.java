93b6d4a6bddc0658c30a39cd4a05ebc9d63a7be1
eitan.aizek <eitan.aizek@techork.com>
2024-01-20 22:10:00 +0500

    m: added GraderContextTest

---
+++ memed-core/src/test/java/kz/ruanjian/memed/util/grader/GraderContextTest.java
@@ -0,0 +1,60 @@
+package kz.ruanjian.memed.util.grader;
+
+import kz.ruanjian.memed.model.Question;
+import kz.ruanjian.memed.pojo.answer.MultipleChoiceAnswer;
+import kz.ruanjian.memed.pojo.answer.SingleChoiceAnswer;
+import kz.ruanjian.memed.util.data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class GraderContextTest {
+
+  @Mock
+  SingleChoiceAnswerGrader singleChoiceAnswerGrader;
+
+  @InjectMocks
+  GraderContext graderContext;
+
+  DataGenerator dataGenerator;
+
+  @BeforeEach
+  void setUp() {
+    dataGenerator = new DataGenerator();
+  }
+
+  @Test
+  void grade_shouldThrowGraderException_whenNoMatchingGrader() {
+    Question question = new Question();
+    MultipleChoiceAnswer answer = dataGenerator.generateMultipleChoiceAnswer();
+    question.setAnswer(answer);
+
+    GraderException thrown = assertThrows(GraderException.class, () -> graderContext.grade(question));
+
+    String expectedMessage = "There is no grader to handle question";
+    assertEquals(expectedMessage, thrown.getMessage());
+  }
+
+  @Test
+  void grade_shouldDoAppropriateOperations_whenQuestionWithSingleChoiceAnswerPassed() {
+    Question question = new Question();
+    SingleChoiceAnswer answer = dataGenerator.generateSingleChoiceAnswer();
+    question.setAnswer(answer);
+
+    int expected = 100;
+    doReturn(expected).when(singleChoiceAnswerGrader).grade(question);
+
+    int actual = graderContext.grade(question);
+
+    assertEquals(expected, actual);
+    verify(singleChoiceAnswerGrader).grade(question);
+  }
+}
