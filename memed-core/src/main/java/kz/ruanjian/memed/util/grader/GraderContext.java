36ef60043e57e4c2478038d8b4cdf53094ade498
eitan.aizek <eitan.aizek@techork.com>
2024-01-20 21:41:33 +0500

    m: removed MultipleChoiceAnswerGrader

--- memed-core/src/main/java/kz/ruanjian/memed/util/grader/GraderContext.java
+++ memed-core/src/main/java/kz/ruanjian/memed/util/grader/GraderContext.java
@@ -10,22 +10,15 @@ public class GraderContext implements Grader {
   private static final String NO_GRADER = "There is no grader to handle question";
 
   private final SingleChoiceAnswerGrader singleChoiceAnswerGrader;
-  private final MultipleChoiceAnswerGrader multipleChoiceAnswerGrader;
 
-  public GraderContext(SingleChoiceAnswerGrader singleChoiceAnswerGrader,
-                       MultipleChoiceAnswerGrader multipleChoiceAnswerGrader) {
+  public GraderContext(SingleChoiceAnswerGrader singleChoiceAnswerGrader) {
     this.singleChoiceAnswerGrader = singleChoiceAnswerGrader;
-    this.multipleChoiceAnswerGrader = multipleChoiceAnswerGrader;
   }
 
   @Override
   public int grade(Question question) {
     if (BlankType.SINGLE_CHOICE.equals(question.getAnswer().getType())) {
       return singleChoiceAnswerGrader.grade(question);
-    }
-
-    if (BlankType.MULTIPLE_CHOICE.equals(question.getAnswer().getType())) {
-      return multipleChoiceAnswerGrader.grade(question);
     }
 
     throw new GraderException(NO_GRADER);
