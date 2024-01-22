ba2444d3064e6355dc26beebe0a2c6eefffec54d
eitan.aizek <eitan.aizek@techork.com>
2024-01-22 13:08:58 +0500

    m: findByUsername_shouldThrowNotFoundException_whenNotExistingLeadUsernamePassed

--- memed-core/src/test/java/kz/ruanjian/memed/service/LeadServiceTest.java
+++ memed-core/src/test/java/kz/ruanjian/memed/service/LeadServiceTest.java
@@ -1,7 +1,9 @@
 package kz.ruanjian.memed.service;
 
 import kz.ruanjian.memed.data.DataGenerator;
+import kz.ruanjian.memed.model.Lead;
 import kz.ruanjian.memed.respository.LeadRepository;
+import kz.ruanjian.memed.service.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class LeadServiceTest {
@@ -31,11 +35,16 @@ class LeadServiceTest {
   }
 
   @Test
-  void findByUsername_should_when1() {
+  void findByUsername_shouldThrowNotFoundException_whenNotExistingLeadUsernamePassed() {
     String username = dataGenerator.generateWord(8);
-    doReturn(Optional.empty()).when(leadService).findByUsername(username);
+    doReturn(Optional.empty()).when(leadRepository).findByUsername(username);
 
+    NotFoundException thrown = assertThrows(NotFoundException.class, () -> leadService.findByUsername(username));
 
+    String expectedMessage = "Lead not found";
+    assertEquals(expectedMessage, thrown.getLocalizedMessage());
+
+    verify(leadRepository).findByUsername(username);
   }
 
   @Test
