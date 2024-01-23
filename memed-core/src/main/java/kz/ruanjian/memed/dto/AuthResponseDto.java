e85bd37f365b4230310f04be5aaec6296c4faaf4
eitan.aizek <eitan.aizek@techork.com>
2024-01-23 22:45:03 +0500

    m: added equals() and hashCode() of AuthResponseDto

--- memed-core/src/main/java/kz/ruanjian/memed/dto/AuthResponseDto.java
+++ memed-core/src/main/java/kz/ruanjian/memed/dto/AuthResponseDto.java
@@ -2,6 +2,8 @@ package kz.ruanjian.memed.dto;
 
 import kz.ruanjian.memed.model.Lead;
 
+import java.util.Objects;
+
 public class AuthResponseDto {
 
   private String token;
@@ -21,5 +23,18 @@ public class AuthResponseDto {
 
   public void setLead(Lead lead) {
     this.lead = lead;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    AuthResponseDto that = (AuthResponseDto) o;
+    return Objects.equals(token, that.token) && Objects.equals(lead, that.lead);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(token, lead);
   }
 }
--- memed-core/src/test/java/kz/ruanjian/memed/service/AuthServiceTest.java
+++ memed-core/src/test/java/kz/ruanjian/memed/service/AuthServiceTest.java
@@ -2,6 +2,8 @@ package kz.ruanjian.memed.service;
 
 import kz.ruanjian.memed.data.DataGenerator;
 import kz.ruanjian.memed.dto.AuthDto;
+import kz.ruanjian.memed.dto.AuthResponseDto;
+import kz.ruanjian.memed.model.Lead;
 import kz.ruanjian.memed.respository.LeadRepository;
 import kz.ruanjian.memed.respository.VisitRepository;
 import kz.ruanjian.memed.security.SecurityManager;
@@ -16,10 +18,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,6 +61,24 @@ class AuthServiceTest {
     doThrow(BadCredentialsException.class).when(authenticationManager).authenticate(authentication);
 
     assertThrows(AuthenticationFailedException.class, () -> authService.login(authDto));
+  }
+
+  @Test
+  void login_shouldReturnAuthResponse_whenSuccessfullyLoggedIn() {
+    Lead lead = dataGenerator.generateLead();
+    AuthDto authDto = dataGenerator.generateAuthDto(lead);
+    doReturn(Optional.of(lead)).when(leadRepository).findByUsername(authDto.getUsername());
+
+    String expectedToken = dataGenerator.generateWord(100);
+    doReturn(expectedToken).when(securityManager).generateToken(lead);
+
+    AuthResponseDto expected = new AuthResponseDto();
+    expected.setLead(lead);
+    expected.setToken(expectedToken);
+
+    AuthResponseDto actual = authService.login(authDto);
+
+    assertEquals(expected, actual);
   }
 
   @Test
