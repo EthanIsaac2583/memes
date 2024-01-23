b74d2ed67d824df63b190e20c698c096f88e0d18
eitan.aizek <eitan.aizek@techork.com>
2024-01-24 00:20:23 +0500

    m: fixed Visit components

--- memed-core/src/main/java/kz/ruanjian/memed/controller/VisitController.java
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/VisitController.java
@@ -1,12 +1,10 @@
 package kz.ruanjian.memed.controller;
 
-import kz.ruanjian.memed.dto.VisitDto;
 import kz.ruanjian.memed.model.Visit;
 import kz.ruanjian.memed.service.VisitService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +26,7 @@ public class VisitController {
   }
 
   @PostMapping
-  public Visit create(@RequestBody VisitDto visit) {
-    return visitService.create(visit);
+  public Visit create() {
+    return visitService.create();
   }
 }
--- memed-core/src/main/java/kz/ruanjian/memed/service/VisitService.java
+++ memed-core/src/main/java/kz/ruanjian/memed/service/VisitService.java
@@ -1,7 +1,5 @@
 package kz.ruanjian.memed.service;
 
-import kz.ruanjian.memed.dto.VisitDto;
-import kz.ruanjian.memed.mapper.VisitMapper;
 import kz.ruanjian.memed.model.Visit;
 import kz.ruanjian.memed.respository.VisitRepository;
 import kz.ruanjian.memed.service.exception.NotFoundException;
@@ -17,11 +15,9 @@ public class VisitService {
   private static final String NOT_FOUND = "Visit not found";
 
   private final VisitRepository visitRepository;
-  private final VisitMapper visitMapper;
 
-  public VisitService(VisitRepository visitRepository, VisitMapper visitMapper) {
+  public VisitService(VisitRepository visitRepository) {
     this.visitRepository = visitRepository;
-    this.visitMapper = visitMapper;
   }
 
   public Visit findById(UUID id) {
@@ -31,14 +27,6 @@ public class VisitService {
   }
 
   @Transactional
-  public Visit create(VisitDto visitDto) {
-    Visit visit = visitMapper.toVisit(visitDto);
-    visit.setCreatedAt(ZonedDateTime.now());
-    visitRepository.save(visit);
-
-    return visit;
-  }
-
   public Visit create() {
     Visit visit = Visit.builder()
       .createdAt(ZonedDateTime.now())
