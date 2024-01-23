268a878b28afbbc2999d6e404dca6e946687d1e2
eitan.aizek <eitan.aizek@techork.com>
2024-01-23 22:48:38 +0500

    m: added Builder to AuthResponseDto

--- memed-core/src/main/java/kz/ruanjian/memed/dto/AuthResponseDto.java
+++ memed-core/src/main/java/kz/ruanjian/memed/dto/AuthResponseDto.java
@@ -9,6 +9,14 @@ public class AuthResponseDto {
   private String token;
   private Lead lead;
 
+  public AuthResponseDto() {
+  }
+
+  private AuthResponseDto(Builder builder) {
+    token = builder.token;
+    lead = builder.lead;
+  }
+
   public String getToken() {
     return token;
   }
@@ -36,5 +44,29 @@ public class AuthResponseDto {
   @Override
   public int hashCode() {
     return Objects.hash(token, lead);
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+
+    private String token;
+    private Lead lead;
+
+    public Builder token(String token) {
+      this.token = token;
+      return this;
+    }
+
+    public Builder lead(Lead lead) {
+      this.lead = lead;
+      return this;
+    }
+
+    public AuthResponseDto build() {
+      return new AuthResponseDto(this);
+    }
   }
 }
