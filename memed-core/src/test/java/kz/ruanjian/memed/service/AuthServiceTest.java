78183c077c92d1a58aa3401792945aca818475ff
eitan.aizek <eitan.aizek@techork.com>
2024-01-23 22:28:41 +0500

    m: setup AuthServiceTest

---
+++ memed-core/src/test/java/kz/ruanjian/memed/service/AuthServiceTest.java
@@ -0,0 +1,53 @@
+package kz.ruanjian.memed.service;
+
+import kz.ruanjian.memed.data.DataGenerator;
+import kz.ruanjian.memed.respository.LeadRepository;
+import kz.ruanjian.memed.respository.VisitRepository;
+import kz.ruanjian.memed.security.SecurityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+  @Mock
+  LeadRepository leadRepository;
+
+  @Mock
+  VisitRepository visitRepository;
+
+  @Mock
+  PasswordEncoder passwordEncoder;
+
+  @Mock
+  SecurityManager securityManager;
+
+  @Mock
+  AuthenticationManager authenticationManager;
+
+  @InjectMocks
+  AuthService authService;
+
+  DataGenerator dataGenerator;
+
+  @BeforeEach
+  void setUp() {
+    dataGenerator = new DataGenerator();
+  }
+
+  @Test
+  void login() {
+  }
+
+  @Test
+  void register() {
+  }
+}
