1e3822b6e3a60a445b4761d8b4c2e6d0fb4b4886
eitan.aizek <eitan.aizek@techork.com>
2024-01-24 11:39:33 +0500

    m: keep changes

--- memed-core/src/test/java/kz/ruanjian/memed/service/TemplateServiceTest.java
+++ memed-core/src/test/java/kz/ruanjian/memed/service/TemplateServiceTest.java
@@ -1,15 +1,23 @@
 package kz.ruanjian.memed.service;
 
+import kz.ruanjian.memed.data.DataGenerator;
 import kz.ruanjian.memed.mapper.TemplateMapper;
 import kz.ruanjian.memed.mapper.TemplateMapperImpl;
 import kz.ruanjian.memed.respository.TemplateRepository;
+import kz.ruanjian.memed.service.exception.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TemplateServiceTest {
@@ -23,8 +31,32 @@ class TemplateServiceTest {
   @Mock
   TaskService taskService;
 
+  @InjectMocks
+  TemplateService templateService;
+
+  DataGenerator dataGenerator;
+
+  @BeforeEach
+  void setUp() {
+    dataGenerator = new DataGenerator();
+  }
+
   @Test
-  void findById() {
+  void findById_should_when1() {
+    Long id = dataGenerator.generateLongId();
+    doReturn(Optional.empty()).when(templateRepository).findById(id);
+
+    NotFoundException thrown = assertThrows(NotFoundException.class, () -> templateService.findById(id));
+
+    String expectedMessage = "Template not found";
+    assertEquals(expectedMessage, thrown.getMessage());
+
+    verify(templateRepository).findById(id);
+  }
+
+  @Test
+  void findById_should_when2() {
+
   }
 
   @Test
