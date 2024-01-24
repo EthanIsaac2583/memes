4e699219838fa2530875f385e1b81e924cd85c94
eitan.aizek <eitan.aizek@techork.com>
2024-01-24 16:59:50 +0500

    m: added QuestionRepository.findByIdAndQuizIdAndVisitId()

--- memed-core/src/main/java/kz/ruanjian/memed/respository/QuestionRepository.java
+++ memed-core/src/main/java/kz/ruanjian/memed/respository/QuestionRepository.java
@@ -1,7 +1,6 @@
 package kz.ruanjian.memed.respository;
 
 import kz.ruanjian.memed.model.Question;
-import kz.ruanjian.memed.model.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>, JpaSpecificationExecutor<Question> {
@@ -16,5 +16,5 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, JpaSp
   @Query("SELECT q.number FROM Question q WHERE q.assessed = false AND q.quiz.id = :quizId ORDER BY q.number LIMIT 1")
   Optional<Long> findFirstAssessableQuestionNumber(@Param("quizId") Long quizId);
 
-  Optional<Question> findByIdAndQuizIdAndVisit(Long id, Long quizId, Visit visit);
+  Optional<Question> findByIdAndQuizIdAndVisitId(Long id, Long quizId, UUID visitId);
 }
