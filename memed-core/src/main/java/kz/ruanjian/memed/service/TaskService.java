884f2c471504dd165e939bee9b658eed90d3fe2e
eitan.aizek <eitan.aizek@techork.com>
2024-01-24 14:07:20 +0500

    m: removed unused TaskService methods

--- memed-core/src/main/java/kz/ruanjian/memed/service/TaskService.java
+++ memed-core/src/main/java/kz/ruanjian/memed/service/TaskService.java
@@ -4,9 +4,6 @@ import kz.ruanjian.memed.dto.TaskDto;
 import kz.ruanjian.memed.mapper.TaskMapper;
 import kz.ruanjian.memed.model.Task;
 import kz.ruanjian.memed.respository.TaskRepository;
-import kz.ruanjian.memed.service.exception.NotFoundException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +13,6 @@ import java.util.Set;
 @Service
 public class TaskService {
 
-  private static final String NOT_FOUND_EXCEPTION = "Task not found";
-
   private final TaskRepository taskRepository;
   private final TaskMapper taskMapper;
 
@@ -26,32 +21,14 @@ public class TaskService {
     this.taskMapper = taskMapper;
   }
 
-  public Task findById(Long id) {
-    return taskRepository.findById(id)
-      .orElseThrow(() -> new NotFoundException(NOT_FOUND_EXCEPTION));
-  }
-
   public Set<Task> findAllById(Set<Long> ids) {
     return new HashSet<>(taskRepository.findAllById(ids));
   }
 
-  public Page<Task> findAll(Pageable pageable) {
-    return taskRepository.findAll(pageable);
-  }
-
   @Transactional
   public void save(TaskDto taskDto) {
-    Task task = mapToTask(taskDto);
+    Task task = taskMapper.toTask(taskDto);
 
     taskRepository.save(task);
-  }
-
-  @Transactional
-  public void deleteById(Long id) {
-    taskRepository.deleteById(id);
-  }
-
-  private Task mapToTask(TaskDto taskDto) {
-    return taskMapper.toTask(taskDto);
   }
 }
