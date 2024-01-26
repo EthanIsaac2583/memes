98e367d3eb3ab44281a6fa570714068b5d7d7552
eitan.aizek <eitan.aizek@techork.com>
2024-01-26 11:04:32 +0500

    m: QuestionRepositoryTest

---
+++ memed-core/src/test/java/kz/ruanjian/memed/config/PrepareDbTestExecutionListener.java
@@ -0,0 +1,29 @@
+package kz.ruanjian.memed.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+
+public class PrepareDbTestExecutionListener implements TestExecutionListener {
+
+  private static final String DB_URL_PROPERTY = "spring.datasource.url";
+  private static final String DB_USERNAME_PROPERTY = "spring.datasource.username";
+  private static final String DB_PASSWORD_PROPERTY = "spring.datasource.password";
+
+  @Override
+  public void beforeTestMethod(TestContext testContext) throws Exception {
+    Environment environment = testContext.getApplicationContext().getEnvironment();
+
+    String url = environment.getProperty(DB_URL_PROPERTY);
+    String username = environment.getProperty(DB_USERNAME_PROPERTY);
+    String password = environment.getProperty(DB_PASSWORD_PROPERTY);
+
+    Flyway flyway = Flyway.configure()
+      .dataSource(url, username, password)
+      .cleanDisabled(false)
+      .load();
+
+    TestExecutionListener.super.beforeTestMethod(testContext);
+  }
+}
--- memed-core/src/test/java/kz/ruanjian/memed/respository/QuestionRepositoryTest.java
+++ memed-core/src/test/java/kz/ruanjian/memed/respository/QuestionRepositoryTest.java
@@ -1,19 +1,44 @@
 package kz.ruanjian.memed.respository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kz.ruanjian.memed.config.PrepareDbTestExecutionListener;
+import kz.ruanjian.memed.pojo.coverter.AnswerConverter;
+import kz.ruanjian.memed.pojo.json.PojoJson;
+import kz.ruanjian.memed.util.json.JsonUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(value = {ObjectMapper.class, JsonUtil.class, PojoJson.class, AnswerConverter.class})
+@TestExecutionListeners(value = {PrepareDbTestExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 class QuestionRepositoryTest {
 
   @Autowired
   QuestionRepository questionRepository;
 
   @Test
-  void findFirstAssessableQuestionNumber() {
+  @Sql(value = "classpath:db/question/initial.sql")
+  void findFirstAssessableQuestionNumber1() {
+    Optional<Long> expected = Optional.of(1L);
+    Optional<Long> actual = questionRepository.findFirstAssessableQuestionNumber(1L);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @Sql(value = "classpath:db/question/initial.sql")
+  void findFirstAssessableQuestionNumber2() {
+    Optional<Long> expected = Optional.empty();
+    Optional<Long> actual = questionRepository.findFirstAssessableQuestionNumber(2L);
+
+    assertEquals(expected, actual);
   }
 }
