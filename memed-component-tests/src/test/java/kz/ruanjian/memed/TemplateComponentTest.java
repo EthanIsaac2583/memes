be361f793cc4ecfa1d9e507dd551cbf4ce34fecc
eitan.aizek <eitan.aizek@techork.com>
2024-01-27 14:12:57 +0500

    m: added TemplateComponentTest

--- memed-component-tests/src/test/java/kz/ruanjian/memed/TemplateComponentTest.java
+++ memed-component-tests/src/test/java/kz/ruanjian/memed/TemplateComponentTest.java
@@ -1,7 +1,14 @@
 package kz.ruanjian.memed;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.SeedStrategy;
 import kz.ruanjian.memed.config.AbstractComponentTest;
+import org.junit.jupiter.api.Test;
 
+@DataSet(value = "dataset/template/initial.yml", strategy = SeedStrategy.INSERT)
+class TemplateComponentTest extends AbstractComponentTest {
 
-public class TemplateComponentTest extends AbstractComponentTest {
+  @Test
+  void test1() {
+  }
 }
