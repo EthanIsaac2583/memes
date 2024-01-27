24c660ef68786c01db9f2bc5617cddae5e178be2
eitan.aizek <eitan.aizek@techork.com>
2024-01-27 23:13:59 +0500

    m: added pre authorize

--- memed-core/src/main/java/kz/ruanjian/memed/controller/TaskController.java
+++ memed-core/src/main/java/kz/ruanjian/memed/controller/TaskController.java
@@ -3,13 +3,14 @@ package kz.ruanjian.memed.controller;
 import jakarta.validation.Valid;
 import kz.ruanjian.memed.dto.TaskDto;
 import kz.ruanjian.memed.service.TaskService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/tasks")
+@RequestMapping("/api/v1/private/tasks")
 public class TaskController {
 
   private final TaskService taskService;
@@ -19,6 +20,7 @@ public class TaskController {
   }
 
   @PostMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public void create(@RequestBody @Valid TaskDto task) {
     taskService.save(task);
   }
