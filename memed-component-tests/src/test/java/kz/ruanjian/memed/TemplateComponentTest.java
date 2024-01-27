7a80c5eba82ff9c0c6f24f99b0a5acee3878c87a
eitan.aizek <eitan.aizek@techork.com>
2024-01-27 14:18:16 +0500

    m: added TemplateComponentTest.findAll_shouldReturnTemplatePage_whenRequested()

--- memed-component-tests/src/test/java/kz/ruanjian/memed/TemplateComponentTest.java
+++ memed-component-tests/src/test/java/kz/ruanjian/memed/TemplateComponentTest.java
@@ -1,14 +1,37 @@
 package kz.ruanjian.memed;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.core.api.dataset.SeedStrategy;
 import kz.ruanjian.memed.config.AbstractComponentTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @DataSet(value = "dataset/template/initial.yml", strategy = SeedStrategy.INSERT)
 class TemplateComponentTest extends AbstractComponentTest {
 
+  private static final String URI = "/api/v1/templates";
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  WebTestClient webTestClient;
+
+  @BeforeEach
+  void setUp() {
+    webTestClient = WebTestClient.bindToServer()
+      .baseUrl(getServiceUrl(""))
+      .build();
+  }
+
   @Test
-  void test1() {
+  void findAll_shouldReturnTemplatePage_whenRequested() {
+    webTestClient.get()
+      .uri(URI)
+      .exchange()
+      .expectStatus()
+      .isForbidden();
   }
 }
